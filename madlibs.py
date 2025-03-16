a4736aaab7e6f1094565f67860be468b8eda74dd
matheushage1 <matheushagecontato@gmail.com>
2025-03-16 12:44:11 -0300

    final commit for madlibs - the start

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,10 @@
-youtuber = ""
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
 
-print(f"subscribe to my channel in youtube: {youtuber}")
+print("==============================")
+
+madlib = f"Computer programming is so {adj}! I am {verb1} to learn much more than {verb2}. I know that {famous_person} is also studying for fun."
+
+print(madlib)
